7c9165d679becffe7322a09454e492c71962946e
Renato Guimarães <renatoguidev@gmail.com>
2023-01-17 16:30:02 -0300

    17/01/2023

--- 8_REACT-ROUTER/reactrouter/src/App.js
+++ 8_REACT-ROUTER/reactrouter/src/App.js
@@ -1,16 +1,19 @@
 import './App.css';
 
 // 1° Config React Router
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
 
 // Pages
 import Home from './pages/Home';
 import About from './pages/About';
 import Product from './pages/Product';
 import Info from './pages/Info';
+import NotFound from './pages/NotFound';
 
 // Components
 import Navbar from './components/Navbar';
+import SearchForm from './components/SearchForm';
+import Search from './pages/Search';
 
 function App() {
   return (
@@ -19,12 +22,20 @@ function App() {
       <BrowserRouter>
       {/* 2° Links com React Router */}
         <Navbar />
+        {/* 9° Search Params */}
+        <SearchForm />
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/about' element={<About />} />
           {/* 4° Rota Dinâmica */}
           <Route path='/products/:id' element={<Product />} />
           <Route path='/products/:id/info' element={<Info />} />
+          {/* 9° Search Params */}
+          <Route path='/search' element={<Search />} />
+          {/* 10° Redirect */}
+          <Route path='/company' element={<Navigate to='/about' />} />
+          {/* 7° Erro 404 */}
+          <Route path='*' element={<NotFound />} />
         </Routes>
       </BrowserRouter>
     </div>
--- 8_REACT-ROUTER/reactrouter/src/components/Navbar.js
+++ 8_REACT-ROUTER/reactrouter/src/components/Navbar.js
@@ -1,15 +1,20 @@
 // 2° Links com React Router
 import "./Navbar.css"
 
-import { Link } from "react-router-dom"
+import { Link, NavLink } from "react-router-dom"
 
 const Navbar = () => {
   return (
     <nav>
         {/* Link substitui a tag 'a' do HTML */}
-        <Link to='/' >Home</Link>
-        <Link to='/about' >Sobre</Link>
-        <Link to='/products' >Produtos</Link>
+        {/*<Link to='/' >Home</Link>
+        <Link to='/about' >Sobre</Link>*/}
+
+        {/* 8° Link Ativo */}
+        <NavLink to='/' 
+        // className= {({ isActive }) => (isActive ? 'esta-ativo' : 'nao-ativo') }
+        >Home</NavLink>
+        <NavLink to='/about' >Sobre</NavLink>
     </nav>
   )
 }
